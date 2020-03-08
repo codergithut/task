afce32d35076dddab3393d5f9fcd37f889164699
田剑 <tj1468195034@gmail.com>
2020-03-08 16:49:04 +0800

    start

---
+++ src/main/java/com/factory/task/config/WebSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.factory.task.config;
+
+import com.factory.task.interceptor.AuthInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebSecurityConfig implements WebMvcConfigurer {
+
+    @Bean
+    public AuthInterceptor getSecurityInterceptor() {
+        return new AuthInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        InterceptorRegistration addInterceptor = registry.addInterceptor(getSecurityInterceptor());
+
+        // 排除配置
+        addInterceptor.excludePathPatterns("/error");
+        addInterceptor.excludePathPatterns("/login**");
+        addInterceptor.excludePathPatterns("/user**");
+
+        // 拦截配置
+        addInterceptor.addPathPatterns("/**");
+    }
+}
+
