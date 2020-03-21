890d91fbe43bce2d93a240d16424220c0d484903
田剑 <tj1468195034@gmail.com>
2020-03-21 13:56:37 +0800

    start

--- src/main/java/com/factory/task/interceptor/AuthInterceptor.java
+++ src/main/java/com/factory/task/interceptor/AuthInterceptor.java
@@ -1,8 +1,6 @@
 package com.factory.task.interceptor;
 
 import com.alibaba.fastjson.JSON;
-import com.factory.task.data.user.UserInfoData;
-import com.factory.task.util.ConstantUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
