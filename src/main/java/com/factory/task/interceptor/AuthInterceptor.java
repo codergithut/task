455434136824d8941ac3671ab56943d238448ece
田剑 <tj1468195034@gmail.com>
2020-03-21 13:56:21 +0800

    start

--- src/main/java/com/factory/task/interceptor/AuthInterceptor.java
+++ src/main/java/com/factory/task/interceptor/AuthInterceptor.java
@@ -1,5 +1,6 @@
 package com.factory.task.interceptor;
 
+import com.alibaba.fastjson.JSON;
 import com.factory.task.data.user.UserInfoData;
 import com.factory.task.util.ConstantUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,11 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.factory.task.util.ConstantUtils.TOKEN;
 import static com.factory.task.util.ConstantUtils.USERINFO;
@@ -22,23 +28,30 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
 
     @Override
     public boolean preHandle(HttpServletRequest request,
-                             HttpServletResponse response, Object handler) {
+                             HttpServletResponse response, Object handler) throws IOException {
         String token = request.getHeader(TOKEN);
         System.out.println("filter token:" + token);
         System.out.println("filter userCode:" + authResource.getUserCodeByToken(token));
         String uri = request.getRequestURI();
         if(token == null) {
-            response.setContentType("application/json;charset=utf-8");
-            response.setStatus(403);
+            failResponseData(response);
             return false;
         }
         if(StringUtils.isEmpty(authResource.getUserCodeByToken(token))) {
-            response.setContentType("application/json;charset=utf-8");
-            response.setStatus(403);
+            failResponseData(response);
             return false;
         }
         request.getSession().setAttribute(USERINFO, authResource.getUserCodeByToken(token));
         //return authResource.isUserAuth(userInfo.getUserName(), uri);
         return true;
+    }
+
+    private void failResponseData(HttpServletResponse response) throws IOException {
+        response.setContentType("application/json;charset=utf-8");
+        response.setStatus(403);
+        PrintWriter writer = response.getWriter();
+        Map<String, String> map = new HashMap<>();
+        map.put("error", "token is null or lose effectiveness");
+        writer.write(JSON.toJSONString(map));
     }
 }
