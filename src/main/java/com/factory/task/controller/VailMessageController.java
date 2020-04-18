a627bcb5a668dfe600fde6738a922da18d842cd3
田剑 <tj1468195034@gmail.com>
2020-04-18 16:48:22 +0800

    start

--- src/main/java/com/factory/task/controller/VailMessageController.java
+++ src/main/java/com/factory/task/controller/VailMessageController.java
@@ -1,15 +1,46 @@
 package com.factory.task.controller;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
 /**
  * Created by tianjian on 2020/4/18.
  */
 public class VailMessageController {
-    @GetMapping("/message")
-    public String valilMessage() {
 
-        return "haha";
+    private String token = "3fb2e6f82b96";
+
+    @GetMapping("/message")
+    public String valilMessage(@RequestParam("signature") String signature,
+                               @RequestParam("timestamp") String timestamp,
+                               @RequestParam("nonce") String nonce,
+                               @RequestParam("echostr") String echostr) {
+        if(validMessageUtil(timestamp, nonce, signature)) {
+            return echostr;
+        }
+        return "";
+    }
+
+    private boolean validMessageUtil(String timestamp, String nonce, String signature) {
+        List<String> valMessage = new ArrayList<>();
+        valMessage.add(token);
+        valMessage.add(timestamp);
+        valMessage.add(nonce);
+        Collections.sort(valMessage);
+        StringBuffer s = new StringBuffer();
+        valMessage.stream().forEach(e -> {
+            s.append(e);
+        });
+        String sign = DigestUtils.shaHex(s.toString());
+        if(!sign.equals(signature)) {
+            return false;
+        }
+        return true;
     }
 }
