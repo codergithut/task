a87b2ebe3c16ca23ddd937c7f4468c96db61e54d
tianjian <1468195034@qq.com>
2020-05-18 15:24:33 +0800

    s

--- src/main/java/com/factory/task/Example.java
+++ src/main/java/com/factory/task/Example.java
@@ -2,11 +2,13 @@ package com.factory.task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Created by tianjian on 2020/1/11.
  */
 @SpringBootApplication
+@EnableSwagger2
 public class Example {
 
     public static void main(String[] args) {
